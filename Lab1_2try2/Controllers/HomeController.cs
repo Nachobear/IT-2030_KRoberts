7f4cc80fa0e39a0c75ce9847117e270cba9a1b5e
Nachobear <kainerob@gmail.com>
2022-02-19 23:17:46 -0500

    Add project files.

---
+++ Lab1_2try2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Lab1_2try2.Models;
+
+namespace Lab2_1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DA = 0;
+            ViewBag.TL = 0;
+            return View();
+        }
+
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DA = model.CalculateDiscountAmount();
+                ViewBag.TL = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.DA = 0;
+                ViewBag.TL = 0;
+            }
+            return View(model);
+        }
+
+       
+    }
+}
+
